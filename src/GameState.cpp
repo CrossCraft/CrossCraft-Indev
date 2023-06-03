af44cfd0a0407d34cf05e06276ee1afa4931552c
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2023-06-03 18:24:07 -0400

    Init Core and Send Event

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -5,9 +5,13 @@ namespace CrossCraft {
 
 void GameState::on_start() {
     CC_Core_Init();
+
+    // Send initial player position.
+    CC_Event_Push_PlayerUpdate(-1, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, false);
 }
 
 void GameState::on_update(Core::Application* app, double dt) {
+    CC_Core_Update(dt);
 }
 
 void GameState::on_draw(Core::Application* app, double dt) {
